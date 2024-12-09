43194af796d8bd14ffd2fdaeab674f95bdaa3c01
Haril2766 <parmarharil2766@gmail.com>
2024-12-09 11:29:50 +0530

    09/12/2024

---
+++ dotNetCore/Lab-3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program - 1
+
+            //int a = 5;
+            //int b = 0;
+
+            //try
+            //{
+            //    Console.WriteLine(a / b);
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //finally {
+            //    Console.WriteLine(a/b);            
+            //}
+            //Program-2
+
+            //int[] arr = new int[5];
+
+
+            //try
+            //{
+            //    for (int i = 0; i <= arr.Length; i++)
+            //    {
+            //        arr[i] = i;
+            //    }
+            //}
+            //catch (IndexOutOfRangeException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //Program - 3
+
+            //Calculate calculate = new Calculate();
+
+            //calculate.SumOfThree(5, 6, 0);
+            //calculate.SumOfTwo(5, 6);
+
+            //Program - 4
+
+            //Results s1 = new Answer();
+
+            //int x = s1.add(5, 4);
+            //int y = s1.sub(5, 4);
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //Program - 5
+
+            //String x = "Haril Parmar";
+
+            //string p = "Haril ";
+            //string q = "Parmar";
+
+            //String a = x.ToUpper();
+            //Console.WriteLine("Upper Case : "+a);
+
+            //String b = x.ToLower();
+            //Console.WriteLine("Lower Case : " + b);
+
+            //int c = x.Length;
+            //Console.WriteLine("Length of String  : " + c);
+
+            //string d = x.Substring(0,4);
+            //Console.WriteLine("Substring : " + d);
+
+            //String e = String.Concat(p,q);
+            //Console.WriteLine("Concate : " + e);
+
+            //int f = x.IndexOf("a");
+            //Console.WriteLine("Index of a : "+f);
+
+            //Program-6
+
+
+        }
+    }
+}
