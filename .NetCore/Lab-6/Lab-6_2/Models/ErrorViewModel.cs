7a73f592e5ccc5b1ad5fe7f9ce945631b6a9cee4
Haril2766 <parmarharil2766@gmail.com>
2024-12-15 09:43:45 +0530

    15-12-2024

---
+++ .NetCore/Lab-6/Lab-6_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_6_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
