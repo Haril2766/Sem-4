a44bd36dbb38caac9161c9d9c92b32720d29d876
Haril2766 <parmarharil2766@gmail.com>
2025-01-26 19:57:37 +0530

    26-01-2025

---
+++ .NetCore/QUIZ/Quiz-Login-Register/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz_Login_Register.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
